61c7e0f8a8a36ad2c12999e75c125c5a227500b4
Avery Pennarun <apenwarr@gmail.com>
2010-11-21 03:09:21 -0800

    vars.py: it's safe to assume REDO_BASE and REDO_STARTDIR are set correctly.
    This lets us remove an extra call to os.abspath().

--- vars.py
+++ vars.py
@@ -6,9 +6,7 @@ DEPTH = os.environ.get('REDO_DEPTH', '')
 DEBUG = atoi.atoi(os.environ.get('REDO_DEBUG', ''))
 VERBOSE = os.environ.get('REDO_VERBOSE', '') and 1 or 0
 SHUFFLE = os.environ.get('REDO_SHUFFLE', '') and 1 or 0
-STARTDIR = os.environ.get('REDO_STARTDIR', '')
-BASE = os.environ.get('REDO_BASE', '')
-if BASE:
-    BASE = os.path.abspath(BASE)
-    while BASE and BASE.endswith('/'):
-        BASE = BASE[:-1]
+STARTDIR = os.environ['REDO_STARTDIR']
+BASE = os.environ['REDO_BASE']
+while BASE and BASE.endswith('/'):
+    BASE = BASE[:-1]
